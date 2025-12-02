7db1185f1e9324d7fa2e4ac3b00f2841155768f3
HamzaSardar <hamzasardar360@gmail.com>
2025-12-02 11:51:37 +0000

    Added base files, simple training setup.

---
+++ src/data_processing.py
@@ -0,0 +1,20 @@
+import torch
+import torchvision 
+
+def get_dataloaders():
+    transform = torchvision.transforms.Compose(
+        [
+            torchvision.transforms.ToTensor(),
+            torchvision.transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+        ]
+    )
+
+    batch_size = 4
+    train = torchvision.datasets.CIFAR10(root='./data', train=True, download=True, transform=transform)
+    train_loader = torch.utils.data.DataLoader(train, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    test = torchvision.datasets.CIFAR10(root='./data', train=False, download=True, transform=transform)
+    test_loader = torch.utils.data.DataLoader(test, batch_size=batch_size, shuffle=True, num_workers=2)
+
+    return train_loader, test_loader
+    
---
+++ src/train.py
@@ -0,0 +1,47 @@
+import torch
+import torchvision
+import torch.optim as optim
+import torch.nn as nn
+
+from accelerate import Accelerator
+
+from src.model import Classifier
+from src.data_processing import get_dataloaders
+
+
+def train(train_dl, model, optim, loss_fn):
+    for epoch in range(10):
+        loss = 0
+        for i, data in enumerate(train_dl):
+            x, label = data
+            optim.zero_grad()
+
+            out = model(x)
+            loss = loss_fn(out, label)
+
+            if i % 100 == 0:
+                print(f'epoch: {epoch + 1}, loss: {loss.value()}')
+            
+            loss.backward()
+            optim.step()
+
+        print('done')
+               
+
+if __name__=="__main__":
+    # initialise dataloaders and model
+    train_loader, test_loader = get_dataloaders()    
+    model = Classifier()
+
+    # initialise loss and optimiser
+    loss = nn.CrossEntropyLoss()
+    optimiser = optim.Adam(params=model.parameters(), lr=0.001)
+
+    # pass everything to GPU    
+    accelerator = Accelerator()
+    train_loader, test_loader, model, optimiser = accelerator.prepare(
+        train_loader, test_loader, model, optimiser
+    )
+
+    train(train_loader, model=model, optim=optimiser, loss_fn=loss)
+    
